5fcd10cf99a72a938acdb9d5e68fc67d7d45ac17
Raul Rivera <raul259832@gmail.com>
2022-01-17 17:12:13 -0600

    Initial deploy

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "website_endpoint" {
   description = "S3 website endpoint"
-  value       = module.s3_web_app.s3_bucket_website_endpoint
+  value       = "http://${module.s3_web_app.s3_bucket_website_endpoint}"
 }
